d77069c63caf98fc3c77eb286b8632ae2056c12a
Stanislas Quastana <13031988+squasta@users.noreply.github.com>
2022-05-17 10:54:31 +0200

    Update 3-storageaccount.tf

--- Terraform/Lab1_Compte_de_Stockage/3-storageaccount.tf
+++ Terraform/Lab1_Compte_de_Stockage/3-storageaccount.tf
@@ -1,9 +1,6 @@
-## INSERER ICI LE CODE POUR FAIRE : 
-#      --- UN STORAGE ACCOUNT AZURE de type GPv2
-#      --- AVEC UN CONTAINER NOMME tfstate et le TLS activé pour les échanges réseaux
 
-# Indices : https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
-#           https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_container
+# lectures utiles : https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
+#                   https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_container
 
 
 resource "azurerm_storage_account" "Terra-Storage-Backend" {
